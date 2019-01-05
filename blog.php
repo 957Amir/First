3745492bdd718c5473d616294a106f603377c187
Amir <mc170202201@vu.edu.pk>
2019-01-05 13:03:52 +0500

    Edit FILE
    file edit

---
+++ blog.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<style>
+    body{
+        background-color:lightgrey;
+        font-family: monospace;
+        color:blue;
+    }
+    h1 {
+        color:black;
+        font-style:italic;
+        font-weight:bold;
+    }
+    
+    }
+    h2{
+        color:black;
+        font-weight:bold;
+        font-family:sans-serif;
+    }
+</style>
+    <head>
+        <meta charset="utf-8">
+        <title>Project: Blog</title>
+    </head>
+    <body>
+        
+        <h1>Dawn's blog</h1>
+
+        <h2>Contents</h2>
+        <ol>
+            <li><a href = "#boing-1">Boing Boing!</a></li>
+            <li ><a href = "#tech-1">Techcrunch!</a></li>
+        </ol>
+        
+        <h2  id = "boing-1">Boing Boing</h2>
+        <h6>Posted on 26 September 2018</h6>
+        
+        <p>Lego reconstructions of pop videos and cakes baked in the shape of iPods are not generally considered relevant to serious political debate. But even the most earnest bloggers will often take time out of their busy schedule to pass on some titbit of mildly entertaining geek ephemera. No one has done more to promote pointless, yet strangely cool, time-wasting stuff on the net than the editors of Boing Boing (subtitle: A Directory of Wonderful Things). It launched in January 2000 and has had an immeasurable influence on the style and idiom of blogging. But hidden among the pictures of steam-powered CD players and Darth Vader tea towels there is a steely, ultra-liberal political agenda: championing the web as a global medium free of state and corporate control.</p>
+        <p>Boing Boing chronicles cases where despotic regimes have silenced or imprisoned bloggers. It helped channel blogger scorn on to Yahoo and Google when they kowtowed to China's censors in order to win investment opportunities. It was instrumental in exposing the creeping erosion of civil liberties in the US under post-9/11 'Homeland Security' legislation. And it routinely ridicules attempts by the music and film industries to persecute small-time file sharers and bedroom pirates instead of getting their own web strategies in order. It does it all with gentle, irreverent charm, polluted only occasionally with gratuitous smut.</p>
+        <p>
+            Their dominance of the terrain where technology meets politics makes the Boing Boing crew geek aristocracy.
+
+Least likely to post 'Has anyone got a stamp?'
+        </p>
+                <h2  id = "tech-1">Techcrunch</h2>
+          <h6>Posted on 21 September 2017</h6>
+<p>Techcrunch began in 2005 as a blog about dotcom start-ups in Silicon Valley, but has quickly become one of the most influential news websites across the entire technology industry. Founder Michael Arrington had lived through the internet goldrush as a lawyer and entrepreneur before deciding that writing about new companies was more of an opportunity than starting them himself. His site is now ranked the third-most popular blog in the world by search engine Technorati, spawning a mini-empire of websites and conferences as a result. Business Week named Arrington one of the 25 most influential people on the web, and Techcrunch has even scored interviews with Barack Obama and John McCain.</p>
+<p>
+    With a horde of hungry geeks and big money investors online, Techcrunch is the largest of a wave of technology-focused blog publishers to tap into the market - GigaOm, PaidContent and Mashable among them - but often proves more contentious than its rivals, thanks to Arrington's aggressive relationships with traditional media and his conflicts of interest as an investor himself.
+
+
+</p>
+
+        
+        
+    </body>
+</html>
